0dc82d3c7704c064f543dd0e5f7819e83e6fa547
tsu-khan-yuk <62913085+tsu-khan-yuk@users.noreply.github.com>
2020-05-12 18:50:19 +0300

    еще немножко

--- main_9.py
+++ main_9.py
@@ -12,33 +12,36 @@
 ######################################################################################################################
 from PyProjects.Module1 import Mage, Pocket, Item
 
-example = {
-    "brace": {
-        "health": 100,
-        "defence": 20,
-        "dmg": 30
-    },
-    "sword": {
-        "health": 0,
-        "defence": 40,
-        "dmg": 100
-    },
-    "helmet": {
-        "health": 0,
-        "defence": 100,
-        "dmg": 0
-    }
-}
+# todo: прикол со словарями в классах
+
+# example = {
+#     "brace": {
+#         "defence": 20,
+#         "dmg": 30
+#     },
+#     "sword": {
+#         "defence": 40,
+#         "dmg": 100
+#     },
+#     "helmet": {
+#         "defence": 100,
+#         "dmg": 0
+#     }
+# }
 
 
-# loot = Pocket()
-# loot.push_item(Item("gas", 100, 20, 30))
-# loot.push_item(Item("gav", 20, 30, 60))
-# loot.push_item(Item("helmet", 20, 30, 80))
-# print(loot)
+sword = Item("sword", 10, 100)
+shield = Item("shield", 100, 0)
 
-mag = Mage("Gav", 100, 100)
+mag = Mage("Sanya", 100, 100)
+
 print(mag)
+mag.take_item(sword)
+mag.take_item(shield)
+print(mag)
+
+
+
 
 
 
