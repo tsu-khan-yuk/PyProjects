2f8f962431a375637bbd6d3999c861858b407894
tsu-khan-yuk <vania8282125@gmail.com>
2020-05-21 20:02:52 +0300

    deploy of mods needs modification

--- main_11.py
+++ main_11.py
@@ -7,57 +7,95 @@
 #  -> *task 2: Доработайте механику сражения и проверки состояния атакуемого юнита
 ######################################################################################################################
 from random import shuffle
-from PyProjects.Modules.Mage import Mage
+from PyProjects.Modules.Mage import Unit, Mage, check_type
 
 
 class Battle:
-	unit1 = None
-	unit2 = None
-	_ind = None
-	# todo: check struct for power up, can be used 'Item'
-	_power_up = {
-		"Mage": {
-			"defence": 50,
-			"dmg": 20
-		},
-		"Knight": {
-			"defence": 100,
-			"dmg": 60
-		}
-	}
-	
-	# check_type(True, Unit, Unit)
-	def __init__(self, unit1, unit2):
-		self.unit1 = unit1
-		self.unit2 = unit2
-		self._ind = 0
-	
-	# check_type(True, str, str)
-	def set_mods_into_battle(self, location=None, day_part=None):
-		pass
-	
-	def _deploy_mods_into_unit(self, unit):
-		return unit
-	
-	@staticmethod
-	def _first_attack():
-		return shuffle(0, 1)
-	
-	def _index(self):
-		while True:
-			self._ind = 1 if self._ind is 1 else 0
-			yield self._ind
-	
-	def battle(self):
-		# todo: solve problem with index in 'unit' list
-		unit = [self._deploy_mods_into_unit(self.unit1), self._deploy_mods_into_unit(self.unit2)]
-		count = self._index()
-		while True:
-			# todo: refresh methods according to raises in 'Unit'
-			val = next(count)
-			self.unit[val].attack(self.unit2)
+    unit1 = None
+    unit2 = None
+    _loc = None
+    _day_part = None
+    _ind = None
+    # todo: check struct for power up, can be used 'Item'
+    _power_up = {
+        "location": {
+            "forest": {
+                Mage: {
+                    "defence": 50,
+                    "dmg": 60
+                }
+            },
+            "city": {
+                "Mage": {
+                    "defence": 80,
+                    "dmg": 40
+                }
+            },
+            "cornfield": {
+                "Knight": {
+                    "defence": 40,
+                    "dmg": 70
+                }
+            }
+        },
+        "day_part": {
+            "morning": {
+                "Knight": {
+                    "defence": 60,
+                    "dmg": 50
+                }
+            },
+            "noon": {
+                "Mage": {
+                    "defence": 40,
+                    "dmg": 70
+                }
+            },
+            "midday": {
+                "Knight": {
+                    "defence": 70,
+                    "dmg": 50
+                }
+            }
+        }
+    }
+
+    @check_type(True, Unit, Unit)
+    def __init__(self, unit1, unit2):
+        self.unit1 = unit1
+        self.unit2 = unit2
+        self._ind = 0
+
+    @check_type(True, str, str)
+    def set_mods_into_battle(self, location=None, day_part=None):
+        self._loc = location
+        self._day_part = day_part
+
+    def _deploy_mods_into_unit(self, unit):
+        # if self._loc is not None:
+        # 	defence_up = self._power_up[self._loc][]
+        return unit
+
+    def _first_attack(self):
+        unit1 = self._deploy_mods_into_unit(self.unit1)
+        unit2 = self._deploy_mods_into_unit(self.unit2)
+        return shuffle(unit1, unit2)
+
+    def _index(self):
+        while True:
+            self._ind = 1 if self._ind is 0 else 0
+            yield self._ind
+
+    def battle(self):
+        unit = self._first_attack()
+        count = self._index()
+
+        while True:
+            unit[next(count)].attack(unit[next(count)])
 
 
 mage1 = Mage("Vasya")
 mage2 = Mage("Petya")
 bat = Battle(mage1, mage2)
+
+bat.battle()
