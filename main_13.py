d5a0214b028bfea89ab4a9c618a115a4b086c7f0
tsu-khan-yuk <62913085+tsu-khan-yuk@users.noreply.github.com>
2020-05-27 19:21:15 +0300

    modified main_13.py

--- main_13.py
+++ main_13.py
@@ -15,9 +15,9 @@ from re import *
 # AА1234BB, 12 123-45AB, a12345BC
 # r"[A-Z]{2}[0-9]{4}[A-Z]{2}", r"[1-9]{2}\s[0-9]{3}-[0-9]{2}[A-Z]{2}", r"[a-z][0-9]{5}[A-Z]{2}"
 base = [
-    compile(r"[A-Z]{2}[0-9]{4}[A-Z]{2}"),               # AА1234BB
-    compile(r"[1-9]{2}\s[0-9]{3}-[0-9]{2}[A-Z]{2}"),    # 12 123-45AB
-    compile(r"[a-z][0-9]{5}[A-Z]{2}")                   # a12345BC
+    compile(r"[A-Z|А-Я]{2}[0-9]{4}[A-Z|А-Я]{2}"),               # AА1234BB
+    compile(r"[1-9]{2}\s[0-9]{3}-[0-9]{2}[A-Z|А-Я]{2}"),    # 12 123-45AB
+    compile(r"[a-z|а-я][0-9]{5}[A-Z|А-Я]{2}")                   # a12345BC
 ]
 
 
@@ -36,13 +36,37 @@ def parser(string: str) -> "None, str":
     raise Exception("Not found")
 
 
-print(parser("AА1234BB"))
-print(parser("12 123-45AB"))
-print(parser("a12345BC"))
+# print(parser("AА1234BB"))
+# print(parser("12 123-45AB"))
+# print(parser("a12345BC"))
+
+class Parser:
+    _string = None
+    _tokens = None
+    _tpl = r"[0-9]{3}-[0-9]{2}[A-Z]{2}"
+
+    def __init__(self, string: str):
+        if not isinstance(string, str):
+            raise TypeError
+        self._string = string
+        self._tokens = string.split()
+        self._string_processing()
+
+    def _string_processing(self):
+        buff = ""
+        for word in self._tokens:
+            word = self._deleting_punctuality(word)
+            if "-" in word and len(word) is 8 and search(self._tpl, word):
+                tmp = buff + word
+            buff = word
+
+    def _deleting_punctuality(self, word):
+        print(word)
+        word = sub(r"^\W.{6}\W$", "", word)
+        print(word)
+        print()
+        return word
 
 
-
-
-
-
+obj = Parser(",AА1234BB, 12 123-45AB, a12345BC")
 
